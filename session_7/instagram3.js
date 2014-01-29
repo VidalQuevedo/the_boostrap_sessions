590d336ca2ef7d7368ee4aaab340ccfe4283a851
Vidal Quevedo <vidal.e.quevedo@gmail.com>
2014-01-29 13:47:52 -0600

    add instagram3.* files

---
+++ session_7/instagram3.js
@@ -0,0 +1,24 @@
+// $(document).ready(function(){
+// 	$(window).on('scroll', function(){
+// 		var dups = [];
+// 		if($(window).scrollTop() + $(window).height() == $(document).height()){
+// 			for (i=0; i<5; i++){
+// 		 		$($('#content .row')[0]).clone().appendTo("#content");
+// 			}
+// 		}
+// 	});
+// });
+
+$(document).ready(function(){
+	$('#load-more').on('click', function(){
+		var dups = [];
+		// if($(window).scrollTop() + $(window).height() == $(document).height()){
+			for (i=0; i<5; i++){
+		 		var $el = $($('#content .row')[0]).clone();
+		 		$el.hide().appendTo("#content");
+		 		// $el.children('.card-tray').children('.card').children('.card-image').attr('src', 'http://placekitten.com/613/613');
+		 		$el.fadeIn(300);
+			}
+		// }
+	});
+});
