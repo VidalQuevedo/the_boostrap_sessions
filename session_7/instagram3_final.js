b9d89428db7197e26b443b52cd75189bc2b16dfb
Vidal Quevedo <vidal.e.quevedo@gmail.com>
2014-01-29 13:20:34 -0600

    link to instagram3_final.js

--- session_7/instagram3_final.js
+++ session_7/instagram3_final.js
@@ -1,5 +1,16 @@
+// $(document).ready(function(){
+// 	$(window).on('scroll', function(){
+// 		var dups = [];
+// 		if($(window).scrollTop() + $(window).height() == $(document).height()){
+// 			for (i=0; i<5; i++){
+// 		 		$($('#content .row')[0]).clone().appendTo("#content");
+// 			}
+// 		}
+// 	});
+// });
+
 $(document).ready(function(){
-	$(window).on('scroll', function(){
+	$('#load-more').on('click', function(){
 		var dups = [];
 		if($(window).scrollTop() + $(window).height() == $(document).height()){
 			for (i=0; i<5; i++){
