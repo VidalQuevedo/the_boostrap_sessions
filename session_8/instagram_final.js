e709fb48505a851f1d0f3b3aa5b94e07cbd63215
Vidal Quevedo <vidal.e.quevedo@gmail.com>
2014-02-05 11:52:35 -0600

    move randomizer to randomDimension() function

--- session_8/instagram_final.js
+++ session_8/instagram_final.js
@@ -1,13 +1,12 @@
 
 $(document).ready(function(){
 	$('#load-more-btn').on('click', function(){
-		var dimensions = [611,612,613,614,615,616];
 		var dups = [];
 		// if($(window).scrollTop() + $(window).height() == $(document).height()){
 			for (i=0; i<5; i++){
 		 		var $clone = $($('#content .row')[0]).clone();
-		 		var randomX  = dimensions[Math.floor(Math.random() * dimensions.length)].toString();
-		 		var randomY  = dimensions[Math.floor(Math.random() * dimensions.length)].toString();
+		 		var randomX  = randomDimension();
+		 		var randomY  = randomDimension();
 		 	  $clone.find('.card-image img').attr('src', 'http://placekitten.com/' + randomX + '/' + randomY);
 		 		$clone.hide().appendTo("#content");
 		 		$clone.fadeIn(300);
@@ -15,5 +14,8 @@ $(document).ready(function(){
 		// }
 	});
 
-	var random
+	function randomDimension() {
+		var dimensions = [611,612,613,614,615,616];
+		return dimensions[Math.floor(Math.random() * dimensions.length)].toString();
+	}
 });
