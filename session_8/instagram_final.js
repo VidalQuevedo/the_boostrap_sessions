1e8bda0d8e13ac58ff5a492aaed6ad5aec5cfe41
Vidal Quevedo <vidal.e.quevedo@gmail.com>
2014-02-05 11:48:19 -0600

    add radomizer to generate different cat images when 'load more.../ button is pressed

--- session_8/instagram_final.js
+++ session_8/instagram_final.js
@@ -11,14 +11,15 @@
 
 $(document).ready(function(){
 	$('#load-more-btn').on('click', function(){
+		var dimensions = [611,612,613,614,615,616];
 		var dups = [];
 		// if($(window).scrollTop() + $(window).height() == $(document).height()){
 			for (i=0; i<5; i++){
 		 		var $clone = $($('#content .row')[0]).clone();
-
+		 		var rand_x  = dimensions[Math.floor(Math.random() * dimensions.length)].toString();
+		 		var rand_y  = dimensions[Math.floor(Math.random() * dimensions.length)].toString();
+		 	  $clone.find('.card-image img').attr('src', 'http://placekitten.com/' + rand_x + '/' + rand_y);
 		 		$clone.hide().appendTo("#content");
-
-		 		// $clone.children('.card-tray').children('.card').children('.card-image').attr('src', 'http://placekitten.com/613/613');
 		 		$clone.fadeIn(300);
 			}
 		// }
