d23cf682fd08cbfc354b8b3eed9c28c1e7240f1a
Vidal Quevedo <vidal.e.quevedo@gmail.com>
2014-02-05 14:06:09 -0600

    add practice files

---
+++ session_8/instagram_4.js
@@ -0,0 +1,21 @@
+
+$(document).ready(function(){
+	$('#load-more-btn').on('click', function(){
+		var dups = [];
+		// if($(window).scrollTop() + $(window).height() == $(document).height()){
+			for (i=0; i<5; i++){
+		 		var $clone = $($('#content .row')[0]).clone();
+		 		var randomX  = getRandomDimension();
+		 		var randomY  = getRandomDimension();
+		 	  $clone.find('.card-image img').attr('src', 'http://placekitten.com/' + randomX + '/' + randomY);
+		 		$clone.hide().appendTo("#content");
+		 		$clone.fadeIn(300);
+			}
+		// }
+	});
+
+	function getRandomDimension() {
+		var dimensions = [611,612,613,614,615,616];
+		return dimensions[Math.floor(Math.random() * dimensions.length)].toString();
+	}
+});
