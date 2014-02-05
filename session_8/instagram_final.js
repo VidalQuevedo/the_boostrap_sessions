f9e2c365e1ee9e22e1d7d43a24db24b65719d0eb
Vidal Quevedo <vidal.e.quevedo@gmail.com>
2014-02-05 11:28:04 -0600

    rename  as

--- session_8/instagram_final.js
+++ session_8/instagram_final.js
@@ -14,10 +14,12 @@ $(document).ready(function(){
 		var dups = [];
 		// if($(window).scrollTop() + $(window).height() == $(document).height()){
 			for (i=0; i<5; i++){
-		 		var $el = $($('#content .row')[0]).clone();
-		 		$el.hide().appendTo("#content");
-		 		// $el.children('.card-tray').children('.card').children('.card-image').attr('src', 'http://placekitten.com/613/613');
-		 		$el.fadeIn(300);
+		 		var $clone = $($('#content .row')[0]).clone();
+
+		 		$clone.hide().appendTo("#content");
+
+		 		// $clone.children('.card-tray').children('.card').children('.card-image').attr('src', 'http://placekitten.com/613/613');
+		 		$clone.fadeIn(300);
 			}
 		// }
 	});
