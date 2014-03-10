4c27669cbcb9a87a57d5939f865f8a4012dd7948
Vidal Quevedo <vidal.e.quevedo@gmail.com>
2014-03-09 22:44:09 -0500

    Invoke getRandomImages() on load

--- session_8/instagram_final.js
+++ session_8/instagram_final.js
@@ -1,16 +1,19 @@
 $(document).ready(function(){
-	$('#load-more-btn').on('click', function(){
-		var dups = [];
-			for (i=0; i<5; i++){
-		 		var $clone = $($('#content .row')[0]).clone();
-		 		var randomX  = getRandomDimension();
-		 		var randomY  = getRandomDimension();
-		 	  $clone.find('.card-image img').attr('src', 'http://placekitten.com/' + randomX + '/' + randomY);
-		 		$clone.hide().appendTo("#content");
-		 		$clone.fadeIn(300);
-			}
-	});
+	$('#load-more-btn').on('click', getRandomImages);
+	getRandomImages();
 });
+
+function getRandomImages(){
+	var dups = [];
+	for (i=0; i<5; i++){
+		var $clone = $($('#content .row')[0]).clone();
+		var randomX  = getRandomDimension();
+		var randomY  = getRandomDimension();
+		$clone.find('.card-image img').attr('src', 'http://placekitten.com/' + randomX + '/' + randomY);
+		$clone.hide().appendTo("#content");
+		$clone.fadeIn(300);
+	}
+}
 
 function getRandomDimension() {
 	var dimensions = [611,612,613,614,615,616];
